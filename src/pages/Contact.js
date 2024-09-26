0badefc171e2791667de3f8b372315faa7a6dd3a
Your GitHub username here <congtianyue233@gmail.com>
2024-09-26 23:16:38 +0800

    Updated contact info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ import ContactIcons from '../components/Contact/ContactIcons';
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ hi@mldangelo.com"
+    description="Contact Sam Cong via email congtianyue233@gmail.com"
   >
     <article className="post" id="contact">
       <header>
