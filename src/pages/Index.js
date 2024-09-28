635c8a68f0a87b3a172037ff249ed096d1c25d2d
Your GitHub username here <congtianyue233@gmail.com>
2024-09-28 16:37:28 -0500

    Changed name into Sam Cong

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,35 +5,27 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={
-      "Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-      + 'VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni.'
-    }
+    description="Sam Cong's personal website."
   >
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2>
-            <Link to="/">About this site</Link>
+            <Link to="/">Personal Webpage of Sam</Link>
           </h2>
           <p>
             A beautiful, responsive, statically-generated, react application
-            written with modern Javascript.
+            written with modern Javascript (credit to <a href="https://github.com/mldangelo/personal-site">Michael Angelo</a>.)
           </p>
         </div>
       </header>
       <p>
         {' '}
-        Welcome to my website. Please feel free to read more{' '}
+        👋 Welcome to my homepage! Please feel free to read more{' '}
         <Link to="/about">about me</Link>, or you can check out my{' '}
         <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,{' '}
         view <Link to="/stats">site statistics</Link>, or{' '}
         <Link to="/contact">contact</Link> me.
-      </p>
-      <p>
-        {' '}
-        Source available{' '}
-        <a href="https://github.com/mldangelo/personal-site">here</a>.
       </p>
     </article>
   </Main>
